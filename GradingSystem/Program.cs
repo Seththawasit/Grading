bbf3aa50571f80f183af36d6467f44e2036eb2ee
Seththawasit <146754957+Seththawasit@users.noreply.github.com>
2023-10-24 19:42:40 +0700

    Add files via upload

---
+++ GradingSystem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingSystem
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter score: ");
+            int score = Convert.ToInt32(Console.ReadLine());
+            Grading.GradeCalculator(score);
+        }
+    }
+}
